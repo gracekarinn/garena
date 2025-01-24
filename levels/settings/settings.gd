7f5453e78f942d6fed0f5a3c89e9e41125aac097
gracekarinn <gracekarina12@gmail.com>
2025-01-24 20:20:04 +0700

    feat: basic setup for settings

---
+++ levels/settings/settings.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, value)
+
+
+func _on_check_box_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
