19801c96f7875cba39e9427840a7aa2c7037849f
gracekarinn <gracekarina12@gmail.com>
2025-01-25 13:28:24 +0700

    feat: test

--- scripts/data/cv_parser.gd
+++ scripts/data/cv_parser.gd
@@ -2,19 +2,23 @@ extends Node
 
 class_name CVParser
 
-const CV_PATH = "res://assets/data/candidates/candidates.json"
+const CV_PATH = "res://assets/data/candidates/cv_data.json"
 var cv_data: Dictionary = {}
 
 func _ready():
 	load_cv_data()
 
 func load_cv_data() -> void:
+	print("Loading CV data from: ", CV_PATH)  
 	if FileAccess.file_exists(CV_PATH):
 		var file = FileAccess.open(CV_PATH, FileAccess.READ)
 		var json = JSON.parse_string(file.get_as_text())
 		if json and json.has("candidates"):
 			for cv in json.candidates:
 				cv_data[cv.id] = cv
+		print("Loaded CV data: ", cv_data) 
+	else:
+		print("File not found!") 
 
 func get_cv(id: String) -> Dictionary:
 	return cv_data.get(id, {})
