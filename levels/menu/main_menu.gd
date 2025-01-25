0b8e00efc37e4571f092e43f9ac4be30c4bce07f
gracekarinn <gracekarina12@gmail.com>
2025-01-25 13:48:55 +0700

    feat: debugging

--- levels/menu/main_menu.gd
+++ levels/menu/main_menu.gd
@@ -6,11 +6,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-
-
 func _on_start_pressed() -> void:
 	#get_tree().change_scene_to_file()
 	pass
 
 func _on_settings_pressed() -> void:
+	print("p")
 	get_tree().change_scene_to_file("res://levels/settings/settings.tscn")
